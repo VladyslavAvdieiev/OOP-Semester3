2c46107347b1d261471a30af757daf6da1abb7b3
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-01 20:52:41 +0200

    Added user interface sketch

--- LaboratoryWork1/PresentationLayer.GraphicalUserInterface/MainWindow.xaml.cs
+++ LaboratoryWork1/PresentationLayer.GraphicalUserInterface/MainWindow.xaml.cs
@@ -18,11 +18,88 @@ namespace PresentationLayer.GraphicalUserInterface
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
+
+        private void EntitiesTypes_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            entities_Border.BorderBrush = Brushes.White;
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem comboBoxItem = (ComboBoxItem)comboBox.SelectedItem;
+            if (comboBoxItem.Content.ToString() == "- not selected -") {
+                objects_DataGrid.ItemsSource = null;
+                objects_DataGrid.Columns.Clear();
+            }
+        }
+
+        private void SerializationTypes_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            serialization_Border.BorderBrush = Brushes.White;
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e) {
+            if (CheckSelection())
+                switch (((ComboBoxItem)entitiesTypes_ComboBox.SelectedItem).Content.ToString()) {
+                    case "Student":
+                        //TODO
+                        break;
+                    case "Teacher":
+                        //TODO
+                        break;
+                }
+        }
+
+        private bool CheckSelection() {
+            if (entitiesTypes_ComboBox.SelectedIndex == 0)
+                entities_Border.BorderBrush = Brushes.Red;
+            if (serializationTypes_ComboBox.SelectedIndex == 0)
+                serialization_Border.BorderBrush = Brushes.Red;
+            return !(entitiesTypes_ComboBox.SelectedIndex == 0 || serializationTypes_ComboBox.SelectedIndex == 0);
+        }
+
+        private void Read_Button_Click(object sender, RoutedEventArgs e) {
+            if (CheckSelection()) {
+                //TODO 
+                switch (((ComboBoxItem)entitiesTypes_ComboBox.SelectedItem).Content.ToString()) {
+                    case "Student":
+                        //TODO
+                        break;
+                    case "Teacher":
+                        //TODO
+                        break;
+                }
+            }
+        }
+
+        private void Remove_Button_Click(object sender, RoutedEventArgs e) {
+            if (objects_DataGrid.SelectedIndex == -1) {
+                error_TextBox.Text = "Choose any row.";
+                return;
+            }
+            switch (((ComboBoxItem)entitiesTypes_ComboBox.SelectedItem).Content.ToString()) {
+                case "Student":
+                    //TODO
+                    break;
+                case "Teacher":
+                    //TODO
+                    break;
+            }
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e) {
+            if (CheckSelection()) {
+                //TODO
+                switch (((ComboBoxItem)entitiesTypes_ComboBox.SelectedItem).Content.ToString()) {
+                    case "Student":
+                        //TODO
+                        break;
+                    case "Teacher":
+                        //TODO
+                        break;
+                }
+            }
+        }
+
+        private void Exit_Button_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
     }
 }

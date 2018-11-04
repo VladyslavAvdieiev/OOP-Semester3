57635f9e593c46a278328fbe1309c4f6185fc598
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-04 13:15:56 +0200

    Done first task

--- LaboratoryWork4/ConsoleApp/Program.cs
+++ LaboratoryWork4/ConsoleApp/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    delegate int[] Sort(int[] arr);
+
+    class Program {
+
+        static void Main(string[] args) {
+            #region First Task
+            Sort sortDel = new Sort(arr => {
+                int counter = 0;
+                for (int i = 1; i < arr.Length; i++)
+                    for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+                        counter++;
+                        int tmp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = tmp;
+                    }
+                return arr;
+            });
+
+            sortDel(new int[] { 9, 8, 7, 1, 2, 3, 6, 5, 4, 0 });
+            #endregion
+
+            #region Second Task
+
+            #endregion
         }
     }
 }

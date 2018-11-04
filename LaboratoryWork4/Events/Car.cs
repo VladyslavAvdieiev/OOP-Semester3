81a401cb5b68238d84b421ab914bc2a979482427
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-04 14:43:22 +0200

    Added entity with events

---
+++ LaboratoryWork4/Events/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void CarStateHandler(object sender, CarEventArgs e);
+
+    public class Car {
+        private bool canMove;
+        private double _fuel;
+        public event CarStateHandler StartedMoving;
+        public event CarStateHandler StopedMoving;
+        public event CarStateHandler FuelIsOver;
+
+        public double Fuel {
+            get => _fuel;
+            private set {
+                _fuel = value;
+                if (_fuel <= 0) {
+                    FuelIsOver?.Invoke(this, new CarEventArgs("Fuel is over", 0));
+                    StopMoving();
+                }
+            }
+        }
+
+        public double FuelConsumption { get; }
+
+        public Car(double fuel = 20000, double fuelConsumption = 100) {
+            Fuel = fuel;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public void StartMoving() {
+            canMove = true;
+            StartedMoving?.Invoke(this, new CarEventArgs("Car started moving", Fuel));
+            Move();
+        }
+
+        private void Move() {
+            while (canMove) {
+                Thread.Sleep(1000);
+                Fuel -= FuelConsumption;
+                // Console.WriteLine(Fuel);
+            }
+        }
+
+        public void StopMoving() {
+            canMove = false;
+            StopedMoving?.Invoke(this, new CarEventArgs("Car stopped moving", Fuel));
+        }
+
+        public void Refuel(double fuel) {
+            Fuel += fuel;
+        }
+    }
+}
---
+++ LaboratoryWork4/Events/CarEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class CarEventArgs : EventArgs {
+        public string Message { get; }
+        public double CurrentFuel { get; }
+        public CarEventArgs(string message, double currentFuel) {
+            Message = message;
+            CurrentFuel = currentFuel;
+        }
+    }
+}

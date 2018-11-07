0b94fe4130da326a0a01cd2c1e6018ddef3db296
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-07 22:31:46 +0200

    add data access services

---
+++ LaboratoryWork5/BusinessAccessLayer.Services/Services/XMLSerializerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DataAccessLayer.DataAccessors;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer.Services
+{
+    public class XMLSerializerService<T> : DataAccessService<T> {
+        public XMLSerializerService() {
+            _serializer = new XMLSerializer<T>();
+        }
+        public XMLSerializerService(string filePath) {
+            _serializer = new XMLSerializer<T>(filePath);
+        }
+    }
+}

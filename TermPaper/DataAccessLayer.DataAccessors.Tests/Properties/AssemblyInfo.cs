5e437954521ffcf521eb8000a4a939fcd0f442ef
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-13 15:53:14 +0200

    test(DAL): add some tests to xml serializer

---
+++ TermPaper/DataAccessLayer.DataAccessors.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataAccessLayer.DataAccessors.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataAccessLayer.DataAccessors.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c6c4d1b-58cb-4c71-ac1a-680687c18f73")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

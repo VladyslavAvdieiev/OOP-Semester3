0e8bd583209ff26218e25af6d74b22d2fe359f46
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-30 12:22:18 +0200

    feat(GUI): add static class Commands

---
+++ TermPaper/PresentationLayer.GraphicalUserInterface/Services/Commands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PresentationLayer.GraphicalUserInterface
+{
+    public static class Commands {
+        public static RoutedCommand Add { get; set; }
+        public static RoutedCommand Delete { get; set; }
+        public static RoutedCommand Save { get; set; }
+
+        static Commands() {
+            Add = new RoutedCommand("Add", typeof(IngredientsDesigner));
+            Delete = new RoutedCommand("Delete", typeof(IngredientsDesigner));
+            Save = new RoutedCommand("Save", typeof(IngredientsDesigner));
+        }
+    }
+}

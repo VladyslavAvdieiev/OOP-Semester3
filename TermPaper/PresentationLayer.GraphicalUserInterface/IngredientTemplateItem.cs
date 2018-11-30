5cb761db4948f198c19a7a6a700ca63d453eeddb
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-30 13:27:00 +0200

    feat(GUI.Services): add template item for designing of ingredients

---
+++ TermPaper/PresentationLayer.GraphicalUserInterface/IngredientTemplateItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.GraphicalUserInterface
+{
+    public class IngredientTemplateItem {
+        public string Name { get; set; }
+        public string ImageSource { get; set; }
+        public double Cost { get; set; }
+        public double Weight { get; set; }
+
+        public IngredientTemplateItem(string name, double cost, double weight, string imageSource) {
+            Name = name;
+            Cost = cost;
+            Weight = weight;
+            ImageSource = imageSource;
+        }
+    }
+}

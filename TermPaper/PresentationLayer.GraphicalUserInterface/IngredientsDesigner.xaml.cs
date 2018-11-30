dcb8e77aa26aef738a4dbd1468c54be6dc8d36bf
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-11-30 14:18:03 +0200

    feat(GUI.IngredientsDesigner): add markup and implementation of IngredientsDesigner window

--- TermPaper/PresentationLayer.GraphicalUserInterface/IngredientsDesigner.xaml.cs
+++ TermPaper/PresentationLayer.GraphicalUserInterface/IngredientsDesigner.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BusinessAccessLayer.Entities;
+using BusinessAccessLayer.Services;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace PresentationLayer.GraphicalUserInterface
 {
@@ -18,33 +21,68 @@ namespace PresentationLayer.GraphicalUserInterface
     /// Interaction logic for IngredientsDesigner.xaml
     /// </summary>
     public partial class IngredientsDesigner : Window {
+        private bool read;
+        private List<Ingredient> ingredientSource;
+        private DataAccessService<List<Ingredient>> ingredientDataAccessService;
 
         public IngredientsDesigner() {
             InitializeComponent();
+            if (LoadMenusFromDB(Properties.Settings.Default.Ingredients_Path))
+                LoadMenuItems();
+        }
+
+        /// <summary>
+        /// Reading ingredients from xml file
+        /// </summary>
+        private bool LoadMenusFromDB(string path) {
+            try {
+                ingredientDataAccessService = new XmlSerializerService<List<Ingredient>>(path);                             // DEBUG use BAL
+                ingredientSource = ingredientDataAccessService.Read();                                                      // DEBUG use BAL
+                return read = true;
+            } catch (FileNotFoundException e) {
+                MessageBox.Show($"Error:\n{e.Message}\n\nSet path to file in Settings - Edit File Paths",
+                                "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return read = false;
+            }
+        }
+
+        /// <summary>
+        /// Adding ingredients from xml file to MenuItem
+        /// </summary>
+        private void LoadMenuItems() {
+            ingredients_DataGrid.ItemsSource = Converter.ToIngredientTemplateItem(ingredientSource);
         }
 
         private void Add_Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
-
+            e.CanExecute = read;
         }
 
         private void Add_Command_Executed(object sender, ExecutedRoutedEventArgs e) {
-
+            List<IngredientTemplateItem> items = (List<IngredientTemplateItem>)ingredients_DataGrid.ItemsSource;
+            items.Add(new IngredientTemplateItem());
+            ingredients_DataGrid.ItemsSource = null;
+            ingredients_DataGrid.ItemsSource = items;
         }
 
         private void Delete_Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
-
+            e.CanExecute = (ingredients_DataGrid.SelectedIndex != -1) && read;
         }
 
         private void Delete_Command_Executed(object sender, ExecutedRoutedEventArgs e) {
-
+            List<IngredientTemplateItem> items = (List<IngredientTemplateItem>)ingredients_DataGrid.ItemsSource;
+            items.RemoveAt(ingredients_DataGrid.SelectedIndex);
+            ingredients_DataGrid.ItemsSource = null;
+            ingredients_DataGrid.ItemsSource = items;
         }
 
         private void Save_Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
-
+            e.CanExecute = read;
         }
 
         private void Save_Command_Executed(object sender, ExecutedRoutedEventArgs e) {
-
+            ingredientDataAccessService.Write(
+                Converter.ToIngredients((List<IngredientTemplateItem>)ingredients_DataGrid.ItemsSource));                   // DEBUG using BAL
+            MessageBox.Show("Data were written down successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }

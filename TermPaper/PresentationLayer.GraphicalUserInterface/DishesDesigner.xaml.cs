90e4e3e79d896b09f9fabdfd8385649461232c12
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-01 16:12:52 +0200

    feat(GUI.DishesDesigner): add markup and implementation of DishesDesigner

--- TermPaper/PresentationLayer.GraphicalUserInterface/DishesDesigner.xaml.cs
+++ TermPaper/PresentationLayer.GraphicalUserInterface/DishesDesigner.xaml.cs
@@ -21,44 +21,33 @@ namespace PresentationLayer.GraphicalUserInterface
     /// Interaction logic for DishesDesigner.xaml
     /// </summary>
     public partial class DishesDesigner : Window {
-        private bool read;
-        private List<Dish> dishSource;
-        private DataAccessService<List<Dish>> dishDataAccessService;
+        private Dish dishSource;
 
-        public DishesDesigner(int index) {
+        /// <summary>
+        /// Constructor with parameter
+        /// </summary>
+        public DishesDesigner(Dish dish) {
             InitializeComponent();
-            if (LoadDishesFromDB(Properties.Settings.Default.Dishes_Path))
-                LoadDishItems(index);
+            dishSource = dish;
+            LoadDishItems();
+        }
+        
+        /// <summary>
+        /// Adding dishes info to window
+        /// </summary>
+        private void LoadDishItems() {
+            name_TextBox.Text = dishSource.Name;
+            description_TextBox.Text = dishSource.Description;
+            image_TextBox.Text = dishSource.ImageSource;
+            weight_Label.Content = dishSource.Weight;
+            cost_TextBox.Text = dishSource.Cost.ToString();
+            time_TextBox.Text = dishSource.Time.ToString();
+            ingredients_ListBox.ItemsSource = Converter.ToTemplateItem(dishSource.Ingredients);
         }
 
         /// <summary>
-        /// Reading dishes from xml file
+        /// Load image by path from image_TextBox
         /// </summary>
-        private bool LoadDishesFromDB(string path) {
-            try {
-                dishDataAccessService = new XmlSerializerService<List<Dish>>(path);                                         // DEBUG use BAL
-                dishSource = dishDataAccessService.Read();                                                                  // DEBUG use BAL
-                return read = true;
-            } catch (FileNotFoundException e) {
-                MessageBox.Show($"Error:\n{e.Message}\n\nSet path to file in Settings - Edit File Paths",
-                                "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return read = false;
-            }
-        }
-
-        /// <summary>
-        /// Adding didhes from xml file to MenuItem
-        /// </summary>
-        private void LoadDishItems(int index) {
-            name_TextBox.Text = dishSource[index].Name;
-            description_TextBox.Text = dishSource[index].Description;
-            image_TextBox.Text = dishSource[index].ImageSource;
-            weight_Label.Content = dishSource[index].Weight;
-            cost_TextBox.Text = dishSource[index].Cost.ToString();
-            time_TextBox.Text = dishSource[index].Time.ToString();
-            ingredients_ListBox.ItemsSource = Converter.ToTemplateItem(dishSource[index].Ingredients);
-        }
-
         private void Image_TextBox_TextChanged(object sender, TextChangedEventArgs e) {
             try {
                 BitmapImage image = new BitmapImage();
@@ -71,36 +60,54 @@ namespace PresentationLayer.GraphicalUserInterface
             }
         }
 
-        private void Add_Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
-            e.CanExecute = read;
-        }
-
+        /// <summary>
+        /// Open BranchNode window to choose ingredient. Add this ingredient to window
+        /// </summary>
         private void Add_Command_Executed(object sender, ExecutedRoutedEventArgs e) {
-            //List<IngredientTemplateItem> items = (List<IngredientTemplateItem>)ingredients_DataGrid.ItemsSource;
-            //items.Add(new IngredientTemplateItem());
-            //ingredients_DataGrid.ItemsSource = null;
-            //ingredients_DataGrid.ItemsSource = items;
+            BranchNode branchNode = new BranchNode("Ingredients");
+            branchNode.Show();
+            branchNode.Closed += (ss, ee) => {
+                if (branchNode.ChosenIngredient != null) {
+                    List<TemplateItem> items = (List<TemplateItem>)ingredients_ListBox.ItemsSource;
+                    items.Add(Converter.ToTemplateItem(branchNode.ChosenIngredient));
+                    ingredients_ListBox.ItemsSource = null;
+                    ingredients_ListBox.ItemsSource = items;
+                }
+            };
         }
 
+        /// <summary>
+        /// CanExecute event
+        /// </summary>
         private void Delete_Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
-            e.CanExecute = (ingredients_ListBox.SelectedIndex != -1) && read;
+            e.CanExecute = (ingredients_ListBox.SelectedIndex != -1);
         }
 
+        /// <summary>
+        /// Delete selected item from window
+        /// </summary>
         private void Delete_Command_Executed(object sender, ExecutedRoutedEventArgs e) {
-            List<TemplateItem> items = (List<TemplateItem>)ingredients_ListBox.ItemsSource;
-            items.RemoveAt(ingredients_ListBox.SelectedIndex);
-            ingredients_ListBox.ItemsSource = null;
-            ingredients_ListBox.ItemsSource = items;
+            MessageBoxResult result = MessageBox.Show("Do you want to remove this item?", "Deletion",
+                                                  MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes);
+            if (result == MessageBoxResult.Yes) {
+                List<TemplateItem> items = (List<TemplateItem>)ingredients_ListBox.ItemsSource;
+                items.RemoveAt(ingredients_ListBox.SelectedIndex);
+                ingredients_ListBox.ItemsSource = null;
+                ingredients_ListBox.ItemsSource = items;
+            }
         }
 
-        private void Save_Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
-            e.CanExecute = read;
-        }
-
+        /// <summary>
+        /// Override values in dish, which came by parameter in constructor
+        /// </summary>
         private void Save_Command_Executed(object sender, ExecutedRoutedEventArgs e) {
-            //ingredientDataAccessService.Write(
-            //    Converter.ToIngredients((List<IngredientTemplateItem>)ingredients_DataGrid.ItemsSource));                   // DEBUG using BAL
-            //MessageBox.Show("Data were written down successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            dishSource.Name = name_TextBox.Text;
+            dishSource.Description = description_TextBox.Text;
+            dishSource.ImageSource = image_TextBox.Text;
+            dishSource.Cost = Convert.ToDouble(cost_TextBox.Text);
+            dishSource.Time = Convert.ToDouble(time_TextBox.Text);
+            dishSource.Ingredients = Converter.ToIngredients((List<TemplateItem>)ingredients_ListBox.ItemsSource);
+            MessageBox.Show("Data were written down successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }

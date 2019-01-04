b364b77eb48928e3ac5bd16da9269b5592081864
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-04 19:29:25 +0200

    fix(GUI): fix some bugs

--- TermPaper/PresentationLayer.GraphicalUserInterface/DishesDesigner.xaml.cs
+++ TermPaper/PresentationLayer.GraphicalUserInterface/DishesDesigner.xaml.cs
@@ -108,7 +108,7 @@ namespace PresentationLayer.GraphicalUserInterface
         /// </summary>
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
             Regex regex = new Regex("[0-9]|,");
-            e.Handled = regex.IsMatch(e.Text);
+            e.Handled = !regex.IsMatch(e.Text);
         }
     }
 }

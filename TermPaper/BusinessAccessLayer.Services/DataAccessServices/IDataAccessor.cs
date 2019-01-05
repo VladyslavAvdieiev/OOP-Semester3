b7b5e9c33e0c594ffa76ca49bcf356c27ed5da5f
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-05 14:05:17 +0200

    feat(BusinessAccessLayer): add implementation of data access services

--- TermPaper/BusinessAccessLayer.Services/DataAccessServices/IDataAccessor.cs
+++ TermPaper/BusinessAccessLayer.Services/DataAccessServices/IDataAccessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataAccessLayer.DataAccessors;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace BusinessAccessLayer.Services
 {
-    public interface IDataAccessor<T>
-    {
+    public interface IDataAccessor<T> {
+        List<T> Data { get; }
+        ISerializer<List<T>> Serializer { get; }
+        void Clear();
+        void Read();
+        void Write();
     }
 }
--- TermPaper/BusinessAccessLayer.Services/DataAccessServices/XmlSerializerService.cs
+++ TermPaper/BusinessAccessLayer.Services/DataAccessServices/XmlSerializerService.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer.DataAccessors;
 
 namespace BusinessAccessLayer.Services
 {
-    public class XmlSerializerService<T> : IDataAccessor<T>
-    {
+    public class XmlSerializerService<T> : IDataAccessor<T> {
+        
+        public List<T> Data { get; internal set; }
+        public ISerializer<List<T>> Serializer { get; }
+
+        public XmlSerializerService() {
+            Serializer = new XmlSerializer<List<T>>();
+        }
+
+        public XmlSerializerService(string filePath) {
+            Serializer = new XmlSerializer<List<T>>(filePath);
+        }
+
+        public void Clear() {
+            Serializer.Clear();
+        }
+
+        public void Read() {
+            Data = Serializer.Deserialize();
+        }
+
+        public void Write() {
+            Serializer.Serialize(Data);
+        }
     }
 }

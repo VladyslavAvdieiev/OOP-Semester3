f6f2bb8d164deca24190b8d08f1bc39e52b89ed1
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-05 14:40:25 +0200

    feat(BusinessAccessLayer): add implementation of DataEditService

--- TermPaper/BusinessAccessLayer.Services/DataEditServices/DataEditService.cs
+++ TermPaper/BusinessAccessLayer.Services/DataEditServices/DataEditService.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace BusinessAccessLayer.Services
 {
-    public static class DataEditService<TEntity>
-    {  
+    public static class DataEditService<TEntity> {  
+
+        public static bool Add(IDataAccessor<TEntity> dataAccessor, TEntity entity) {
+            dataAccessor.Data.Add(entity);
+            return true;
+        }
+
+        public static bool Modify(IDataAccessor<TEntity> dataAccessor, TEntity currentEntity, TEntity newEntity) {
+            if (dataAccessor.Data.Contains(currentEntity)) {
+                int index = dataAccessor.Data.IndexOf(currentEntity);
+                dataAccessor.Data[index] = newEntity;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool Remove(IDataAccessor<TEntity> dataAccessor, TEntity entity) {
+            return dataAccessor.Data.Remove(entity);
+        }
     }
 }

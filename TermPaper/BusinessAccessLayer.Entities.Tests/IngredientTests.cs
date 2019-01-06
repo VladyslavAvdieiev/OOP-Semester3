1258bc69e6f7bb39e1ec1b4867fc7c23bc3c41e6
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-06 17:31:40 +0200

    test(Ingredient): add tests for Ingredient

--- TermPaper/BusinessAccessLayer.Entities.Tests/IngredientTests.cs
+++ TermPaper/BusinessAccessLayer.Entities.Tests/IngredientTests.cs
@@ -5,9 +5,157 @@ namespace BusinessAccessLayer.Entities.Tests
 {
     public class IngredientTests {
 
-        [Fact]
-        public void Test1() {
+        #region Properties
+        #region Name
+        [Theory]
+        [InlineData("Test name1")]
+        [InlineData("Test name2")]
+        public void Name_SetValueToName_Should_SetNameCorreclty(string expected) {
+            /*Arrange*/
+            string actual;
+            Ingredient ingredient = new Ingredient();
 
+            /*Act*/
+            ingredient.Name = expected;
+            actual = ingredient.Name;
+
+            /*Assert*/
+            Assert.Equal(expected, actual);
         }
+        #endregion
+
+        #region Cost
+        [Theory]
+        [InlineData(5.0)]
+        [InlineData(50.0)]
+        public void Cost_SetValidValueToCost_Should_SetCostCorreclty(double expected) {
+            /*Arrange*/
+            double actual;
+            Ingredient ingredient = new Ingredient();
+
+            /*Act*/
+            ingredient.Cost = expected;
+            actual = ingredient.Cost;
+
+            /*Assert*/
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(-5.0)]
+        [InlineData(-50.0)]
+        public void Cost_SetInvalidValueToCost_Should_ThrowFormatExeption(double expected) {
+            /*Arrange*/
+            Ingredient ingredient = new Ingredient();
+
+            /*Act & Assert*/
+            Assert.Throws<FormatException>(() => ingredient.Cost = expected);
+        }
+        #endregion
+
+        #region Weight
+        [Theory]
+        [InlineData(5.0)]
+        [InlineData(50.0)]
+        public void Weight_SetValidValueToWeight_Should_SetWeightCorreclty(double expected) {
+            /*Arrange*/
+            double actual;
+            Ingredient ingredient = new Ingredient();
+
+            /*Act*/
+            ingredient.Weight = expected;
+            actual = ingredient.Weight;
+
+            /*Assert*/
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(-5.0)]
+        [InlineData(-50.0)]
+        public void Weight_SetInvalidValueToWeight_Should_ThrowFormatExeption(double expected) {
+            /*Arrange*/
+            Ingredient ingredient = new Ingredient();
+
+            /*Act & Assert*/
+            Assert.Throws<FormatException>(() => ingredient.Weight = expected);
+        }
+        #endregion
+        #endregion
+
+        #region Constructors
+        #region Constructor with parameters
+        [Theory]
+        [InlineData("Test name1", 5.0, 5.0)]
+        [InlineData("Test name2", 50.0, 50.0)]
+        public void ConstructorWithParameters_CreateInstance_Should_SetAllValuesCorrectly(string name, double cost, double weight) {
+            /*Arrange & Act*/
+            Ingredient expected = new Ingredient { Name = name, Cost = cost, Weight = weight };
+            Ingredient actual = new Ingredient(name, cost, weight);
+
+            /*Assert*/
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+        #region Copy constructor
+        [Theory]
+        [InlineData("Test name1", 5.0, 5.0)]
+        [InlineData("Test name2", 50.0, 50.0)]
+        public void CopyConstructor_CreateInstance_Should_SetAllValuesCorrectly(string name, double cost, double weight) {
+            /*Arrange & Act*/
+            Ingredient expected = new Ingredient { Name = name, Cost = cost, Weight = weight };
+            Ingredient actual = new Ingredient(expected);
+
+            /*Assert*/
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+        #endregion
+
+        #region Equals
+        [Theory]
+        [InlineData("Test name1", 5.0, 5.0)]
+        [InlineData("Test name2", 50.0, 50.0)]
+        public void Equals_CompareTwoIdenticalIngredients_Should_ReturnTrue(string name, double cost, double weight) {
+            /*Arrange*/
+            bool actual;
+            Ingredient ingredient1 = new Ingredient { Name = name, Cost = cost, Weight = weight };
+            Ingredient ingredient2 = new Ingredient { Name = name, Cost = cost, Weight = weight };
+
+            /*Act*/
+            actual = ingredient1.Equals(ingredient2);
+
+            /*Assert*/
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [InlineData("Test name1", 5.0, 5.0)]
+        [InlineData("Test name2", 50.0, 50.0)]
+        public void Equals_CompareTwoDifferentIngredients_Should_ReturnFalse(string name, double cost, double weight) {
+            /*Arrange*/
+            bool actual;
+            Ingredient ingredient1 = new Ingredient { Name = name, Cost = cost, Weight = weight };
+            Ingredient ingredient2 = new Ingredient { Name = name + 5, Cost = cost + 5, Weight = weight + 5 };
+
+            /*Act*/
+            actual = ingredient1.Equals(ingredient2);
+
+            /*Assert*/
+            Assert.False(actual);
+        }
+
+        [Theory]
+        [InlineData(new int())]
+        [InlineData(new char())]
+        public void Equals_CompareIngredientWithDifferentType_Should_ThrowFormatExeption<TDiffType>(TDiffType diffType) {
+            /*Arrange*/
+            Ingredient ingredient = new Ingredient();
+
+            /*Act & Assert*/
+            Assert.Throws<FormatException>(() => ingredient.Equals(diffType));
+        }
+        #endregion
     }
 }

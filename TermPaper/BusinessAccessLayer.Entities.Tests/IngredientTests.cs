c05a9bad69a5bac53c64392b0bcbb1ad48b6083b
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-06 14:47:12 +0200

    feat: add test project for BusinessAccessLayer.Entities

---
+++ TermPaper/BusinessAccessLayer.Entities.Tests/IngredientTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BusinessAccessLayer.Entities.Tests
+{
+    public class IngredientTests {
+
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}

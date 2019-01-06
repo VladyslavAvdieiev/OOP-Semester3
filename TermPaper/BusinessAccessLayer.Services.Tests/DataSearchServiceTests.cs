92eabd5f909cf2b8ab272942c3333fe50e570440
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-06 22:38:26 +0200

    feat: add test project for BusinessAccessLayer.Services

---
+++ TermPaper/BusinessAccessLayer.Services.Tests/DataSearchServiceTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BusinessAccessLayer.Services.Tests
+{
+    public class DataSearchServiceTests {
+
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}

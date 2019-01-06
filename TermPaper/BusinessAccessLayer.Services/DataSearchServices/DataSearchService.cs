b0b4e89172400a7776152c9fc3a52e6191054c89
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-06 22:42:08 +0200

    feat(DataSearchService): add optional search methods

--- TermPaper/BusinessAccessLayer.Services/DataSearchServices/DataSearchService.cs
+++ TermPaper/BusinessAccessLayer.Services/DataSearchServices/DataSearchService.cs
@@ -8,9 +8,28 @@ namespace BusinessAccessLayer.Services
 {
     public static class DataSearchService<TEntity> where TEntity : class {
 
-        public static TEntity SearchByKey(IDataAccessor<TEntity> dataAccessor, string key) {
+        public static TEntity FindByKey(IDataAccessor<TEntity> dataAccessor, string key) {
             if (dataAccessor != null)
                 foreach (var datum in dataAccessor.Data)
+                    if (datum.ToString().Contains(key))
+                        return datum;
+            return null;
+        }
+
+        public static List<TEntity> FindAllByKey(IDataAccessor<TEntity> dataAccessor, string key) {
+            List<TEntity> entities = new List<TEntity>();
+            if (dataAccessor != null)
+                foreach (var datum in dataAccessor.Data)
+                    if (datum.ToString().Contains(key))
+                        entities.Add(datum);
+            return entities;
+        }
+
+        public static TEntity FindLastByKey(IDataAccessor<TEntity> dataAccessor, string key) {
+            List<TEntity> entities = dataAccessor.Data;
+            entities.Reverse();
+            if (dataAccessor != null)
+                foreach (var datum in entities)
                     if (datum.ToString().Contains(key))
                         return datum;
             return null;

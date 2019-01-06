adb1a34892eeeed0cf188cb4c699ccef9253b213
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-06 23:17:43 +0200

    feat(Services.Tests): add test class implements interface IDataAccessor

---
+++ TermPaper/BusinessAccessLayer.Services.Tests/TestDataAccessService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.DataAccessors;
+
+namespace BusinessAccessLayer.Services.Tests
+{
+    public class TestDataAccessService<TData> : IDataAccessor<TData> {
+
+        public List<TData> Data { get; internal set; }
+        public ISerializer<List<TData>> Serializer => throw new NotImplementedException();
+
+        public TestDataAccessService() {
+            Data = new List<TData>();
+        }
+
+        public TestDataAccessService(List<TData> data) {
+            Data = data;
+        }
+
+        public void Clear() {
+            throw new NotImplementedException();
+        }
+
+        public void Read() {
+            throw new NotImplementedException();
+        }
+
+        public bool Write() {
+            throw new NotImplementedException();
+        }
+    }
+}

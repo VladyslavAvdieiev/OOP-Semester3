e98f49ecbfdd162d23b175092dafab7e9e99f49a
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-06 03:01:17 +0200

    feat: add test project for DataAccessLayer.DataAccessors

---
+++ TermPaper/DataAccessLayer.DataAccessors.Tests/XmlSerializerTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace DataAccessLayer.DataAccessors.Tests
+{
+    public class XmlSerializerTests {
+
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}

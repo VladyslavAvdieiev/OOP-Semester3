424a16214b2af788bed50538e98fc0c8de51317a
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-07 23:09:15 +0200

    feat(Services.Tests): add test class TestObject

---
+++ TermPaper/BusinessAccessLayer.Services.Tests/TestObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessAccessLayer.Services.Tests
+{
+    public class TestObject {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+
+        public TestObject(string name, string description, double cost) {
+            Name = name;
+            Description = description;
+            Cost = cost;
+        }
+
+        public override string ToString() {
+            return $"Name:{Name} - Cost:{Cost} - Description:{Description}";
+        }
+    }
+}

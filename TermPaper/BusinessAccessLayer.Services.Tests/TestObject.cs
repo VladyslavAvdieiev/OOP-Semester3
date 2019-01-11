9323a480e1f4a7deb74749b01e38fb9f5d01d3a7
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-11 23:05:58 +0200

    feat(TestObject): override Equals and GetHashCode methods

--- TermPaper/BusinessAccessLayer.Services.Tests/TestObject.cs
+++ TermPaper/BusinessAccessLayer.Services.Tests/TestObject.cs
@@ -14,6 +14,18 @@ namespace BusinessAccessLayer.Services.Tests
             TestValue2 = testValue2;
         }
 
+        public override bool Equals(object obj) {
+            if (obj == null)
+                return false;
+            if (obj is TestObject testObject)
+                return GetHashCode() == testObject.GetHashCode();
+            throw new FormatException("Incoming object is not an 'TestObject' type.");
+        }
+
+        public override int GetHashCode() {
+            return ToString().GetHashCode();
+        }
+
         public override string ToString() {
             return $"TestValue1:{TestValue1} - TestValue2:{TestValue2}";
         }

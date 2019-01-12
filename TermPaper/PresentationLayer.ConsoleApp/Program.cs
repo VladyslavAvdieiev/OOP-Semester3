1388c7cb130cf832e4e5c77838f808e1ff114141
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-01-12 21:26:53 +0200

    feat: add console create console application project for user interface

---
+++ TermPaper/PresentationLayer.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.ConsoleApp
+{
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("The latest version supports GUI is b364b77, 4th January 2019.");
+            Console.ReadLine();
+        }
+    }
+}
